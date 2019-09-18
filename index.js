df51d4d22f34a7388ea61de9544a3449366bb523
Cedric Roux <cedricroux@outlook.fr>
2019-09-18 19:18:56 +0200

    conflits resolved

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function menuPop() {
-    var x = document.getElementById("navMenu");
-    if (x.style.display === "block") {
-      x.style.display = "";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("navMenu");
+  if (x.style.display === "block") {
+    x.style.display = "";
+  } else {
+    x.style.display = "block";
   }
+}
